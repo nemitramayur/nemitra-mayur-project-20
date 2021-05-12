62c8864303aa5aa7171d3bd0e0febb0af22e072a
nemitramayur <81616440+nemitramayur@users.noreply.github.com>
2021-05-12 10:43:34 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,26 +1,29 @@
 var mouse,mouseCheese,mouseDance,mouseCollide;
-var cat,catSit;catWalk,catCollide;
+var cat,catSit,catWalk,catCollide;
 var bg,bgImg;
 
 function preload() { 
-  mouseCheese = loadImage("mouse1.png");
-  mouseDance = loadAnimation("mouse2.png","mouse3.png");
-  mouseCollide = loadImage("mouse4.png",);
+  mouseCheese = loadImage("images/mouse1.png");
+  mouseDance = loadAnimation("images/mouse2.png","images/mouse3.png");
+  mouseCollide = loadAnimation("images/mouse4.png",);
 
-  catSit = loadImage("cat1.png");
-  catWalk = loadAnimation("cat2.png","cat3.png");
-  catCollide = loadImage("cat4.png");
-    bgImg = loadImage("garden.png");
+  catSit = loadAnimation("images/cat1.png");
+  catWalk = loadAnimation("images/cat2.png","images/cat3.png");
+  catCollide = loadAnimation("images/cat4.png");
+    bgImg = loadImage("images/garden.png");
 }
 
 function setup(){
+
     createCanvas(1400,700);
     bg = createSprite(200,200,20,20);
     bg.addImage("bg",bgImg);
+
     mouse = createSprite(100,420,20,20);
     mouse.addAnimation("mouse1",mouseCheese);
     mouse.addAnimation("mouse4",mouseCollide);
     mouse.addAnimation("mouse2,mouse3",mouseDance);
+
     mouse.scale = 0.1;
 
     cat = createSprite(580,420,20,20);
@@ -39,15 +42,17 @@ function draw() {
         cat.velocityX = 0;
         cat.changeAnimation("cat4",catCollide);
     }
-    keyPressed();
+    //keyPressed();
     drawSprites();
 }
 
 function keyPressed(){
-  if(keyDown("left")) {
+  if(keyCode === LEFT_ARROW) {
+    console.log("key down left");
     cat.velocityX = -3;
     cat.changeAnimation("cat2,cat3",catWalk);
     mouse.changeAnimation("mouse2,mouse3",mouseDance);
    } 
 
 }
+   
